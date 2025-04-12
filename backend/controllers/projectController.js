a29ce60bb43f17b22fe8d6b82f20fde2e28f18c2
nirjana <sujatag391@gmail.com>
2025-04-12 23:53:52 +0545

    feat: delete project endpoint

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -1,4 +1,5 @@
 import Project from "../models/project.js";
+import Task from "../models/task.js";
 
 const getProjects = async (req, res) => {
   try {
@@ -20,4 +21,31 @@ const createProject = async (req, res) => {
   }
 };
 
-export { getProjects, createProject };
+const deleteProject = async (req, res, next) => {
+  const { projectId } = req.params;
+
+  try {
+    const tasks = await Task.find({ projectId });
+
+    const deletedProject = await Project.findByIdAndDelete(projectId);
+
+    if (!deletedProject) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    if (tasks.length > 0) {
+      await Task.deleteMany({ projectId });
+      res
+        .status(200)
+        .json({ message: "Project and associated tasks deleted successfully" });
+    } else {
+      res
+        .status(200)
+        .json({ message: "No tasks found, deleting project only" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { getProjects, createProject, deleteProject };
